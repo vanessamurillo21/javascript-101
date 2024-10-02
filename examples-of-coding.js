55ecd5d668903bc7854ae80758f265f96752b95c
vanessamurillo21 <vanesamurillo.murillo1@gmail.com>
2024-10-02 15:44:43 -0700

    Update and rename examples-of-coding to examples-of-coding.js

--- examples-of-coding.js
+++ examples-of-coding.js
@@ -1,10 +1,11 @@
-//Mantipulate Arrays with pop()
+//Mantipulate Arrays with pop(); pop() removes the final element in the array
 var ourArray = [1,2,3];
 var removedFromOurArray = ourArray.pop(); //removedFromOurArray now equals 3, and ourArray now equals [1,2]
 //SetUp
 var myArray = [["John", 23], ["cat", 2]];
 //Change code below 
-var removedFromMyArray;
+var removedFromMyArray = myArray.pop();
+console.log(myArray) // Answer is [["John", 23]]
 
 
 //Manipulate Arrays with shift()
@@ -13,4 +14,4 @@ var removedFromOurArray = ourArray.shift(); //removedFromOurArray now equals "St
 //Setup
 var Array = [["John", 23], ["dog", 3]];
 //Change code below 
-var removedFromMyArray;
+var removedFromMyArray = ;
