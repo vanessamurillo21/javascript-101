9c29dfcccf0283375d25dfec9f6bb073113f8a56
vanessamurillo21 <vanesamurillo.murillo1@gmail.com>
2024-10-02 15:50:13 -0700

    Update examples-of-coding.js

--- examples-of-coding.js
+++ examples-of-coding.js
@@ -8,10 +8,11 @@ var removedFromMyArray = myArray.pop();
 console.log(myArray) // Answer is [["John", 23]]
 
 
-//Manipulate Arrays with shift()
+//Manipulate Arrays with shift(); shift() removes the inital element in the array 
 var ourArray = ["Stimpson", "J", ["cat"]];
 var removedFromOurArray = ourArray.shift(); //removedFromOurArray now equals "Stimpson" and ourArray now equals ["J", ["cat"]];
 //Setup
 var Array = [["John", 23], ["dog", 3]];
 //Change code below 
-var removedFromMyArray = ;
+var removedFromMyArray = myArray.shift();
+console.log(myArray) //myArray is [["dog", 3]]; removedFromMyArray is now [["John", 23]];
