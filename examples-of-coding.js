d58f337652a464639cdbdb17b40b2333e6eef9f0
vanessamurillo21 <vanesamurillo.murillo1@gmail.com>
2024-10-08 19:39:33 -0700

    Update examples-of-coding.js

--- examples-of-coding.js
+++ examples-of-coding.js
@@ -42,6 +42,71 @@ function reusableFunction() {
 reusableFunction(); //Should see "Heyya, World" and "Hi World"
 
 //Passing Values to Functions with Arguments
+function ourFunctionWithArgs(a, b) { //Here we defined a function with this command
+  console.log(a - b); 
+}
+ourFunctionWithArgs(10, 5); //We will then pass this information into the function. nOutput is 5
+//Change below 
+function functionWithArgs(a, b) {
+  console.log(a + b); 
+}
+functionWithArgs(10, 5) //Console answer is 5 and 15
+
+//Global Scope and Functions 
+//Scope: This refers to the visibility of variables; variables defined outside of a function block have global scope
+//Global Scope: They can be seen everywhere in your Javascript code
+//Declare your variable here 
+var myGlobal = 10; //This is a Global Scope. This is set outside of a function, we can see it anywhere in the whole code even in function fun1() and fun2()
+function fun1(){
+  //Assign 5 to oopsGlobal Here 
+  oopsGlobal = 5; //Normally if you do use var keyword, since this is within in functions, it will be scoped to that function
+                  //Since there is no "var" like in "var oopsGlobal," then, that means you can access it anywhere else in the program including here
+}
+//Only change code above this line
+function fun2() { 
+  var output = "";
+  if (typeof myGlobal != "undefined") { //myGlobal referenced here highlighted in white
+    output += "myGlobal: " + myGlobal; //And here too 
+  } 
+  if (typeof oopsGlobal != "undefined") { // != means that "it will not equal"; therefore myGlobal will not equal "undefined"
+    output += "oopsGlobal: " + oopsGlobal; //If we put "var oopsGlobal" then this line 72 would be exist; and oopsGlobal WOULD equal "undefined"
+  }
+  console.log(output); //Console Answer: myGlobal is 10 and oopsGlobal is 5
+}
+fun1();
+fun2();
+
+//Local Scope and Function
+  //Variables which are declared within a function as well as the function parameters have local scope; meaning that they are only visible within the function. 
+function myLocalScope() { 
+  var myVar = //if we declare variable here 
+  console.log(myVar);
+}
+myLocalScope();
+console.log(myVar); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
